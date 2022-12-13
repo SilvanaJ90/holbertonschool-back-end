ee8a6d07ea06925020aceb1c103cf85e3cadd496
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-12 21:04:42 -0500

    update-task0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python3
 """  using this REST API, for a given employee ID, returns information about his/her TODO """
 import requests
+import sys
 
-def gathe_data_api():
-    pass
+users_url = "https://jsonplaceholder.typicode.com/users"
+todos_url = "https://jsonplaceholder.typicode.com/todos"
+
+
+def gathe_data_api(id):
+    """ Doc  """
+
+    todos_count = 0
+    todos_done = 0
+    employee_name = None
+    employee_id = 0
+
+    resp = requests.get(users_url).json()
+
+
+    for i in resp:
+        if i['id'] == id:
+            name = i['name']
 
 if __name__ == "__main__":
     gathe_data_api()
