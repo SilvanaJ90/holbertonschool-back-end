60d392f35fe19815168fffa63a2614edc0c07ea8
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-14 01:14:49 -0500

    update

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -15,17 +15,17 @@ def user_info():
     """ Doc """
 
     response = requests.get(todos_url).json()
-
+    final_json = {}
     ourdata = []
     for i in response:
-        if i['userId'] == id:
-            usr_resp = requests.get(users_url + str(i['userId'])).json()
-            json_entry = {'username': usr_resp[0]['username'],
-                          'task': i['title'], 'completed': i['completed']}
-            ourdata.append(json_entry)
-            final_json = {str(i['userId']): ourdata}
+        url = users_url + str(i['userId'])
+        usr_resp = requests.get(url).json()
+        json_entry = {'username': usr_resp[0]['username'],
+                        'task': i['title'], 'completed': i['completed']}
+        ourdata.append(json_entry)
+    final_json[str(i['userId'])] = ourdata
 
-    with open("todo_all_employees.json", 'w')as f:
+    with open("todo_all_employees.json", mode="w") as f:
         f.write(json.dumps(final_json))
 
 
