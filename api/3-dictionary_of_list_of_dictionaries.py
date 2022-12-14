5ed065ff71f98b73b81a8d053ac5e894d697cf04
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-14 00:52:41 -0500

    update

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -11,23 +11,23 @@ users_url = "https://jsonplaceholder.typicode.com/users?id="
 todos_url = "https://jsonplaceholder.typicode.com/todos"
 
 
-def user_info(id):
+def user_info():
     """ Doc """
 
     response = requests.get(todos_url).json()
-
+    final_json = {}
     ourdata = []
     for i in response:
         if i['userId'] == id:
             usr_resp = requests.get(users_url + str(i['userId'])).json()
-            json_entry = {'task': i['title'], 'completed': i[
-                'completed'], 'username': usr_resp[0]['username']}
+            json_entry = {'username': usr_resp[0]['username'],
+                          'task': i['title'], 'completed': i['completed']}
             ourdata.append(json_entry)
-            final_json = {str(i['userId']): ourdata}
+        final_json[str(i['userId'])] = ourdata
 
-    with open('{}.json'.format(sys.argv[1]), 'w')as f:
+    with open("todo_all_employees.json", mode="w") as f:
         f.write(json.dumps(final_json))
 
 
 if __name__ == "__main__":
-    user_info(int(sys.argv[1]))
+    user_info()
