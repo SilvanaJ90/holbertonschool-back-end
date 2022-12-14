50bb3edcf2fcf0c401d1ec24968eca4e272677c9
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-14 00:27:35 -0500

    update-task2

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -15,18 +15,18 @@ def user_info(id):
     """ Doc """
 
     response = requests.get(todos_url).json()
-    json_entry  = {}
+
     ourdata = []
     for i in response:
         if i['userId'] == id:
             usr_resp = requests.get(users_url + str(i['userId'])).json()
-            json_entry = str(i['userId']), {'task': i['title'], 'completed': i['completed'], 'username': usr_resp[0]['username']}
-
+            json_entry = {'task': i['title'], 'completed': i['completed'], 'username': usr_resp[0]['username']}
             ourdata.append(json_entry)
+            final_json = {str(i['userId']): ourdata}
 
-    student_json = json.dumps(ourdata)
+
     with open('{}.json'.format(sys.argv[1]), 'w')as f:
-        f.write(student_json)
+        f.write(json.dumps(final_json))
 
 
 if __name__ == "__main__":
